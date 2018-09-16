e583539098f398f1c5250f45fb5ec0adb44b6035
jcromeror <camilojeison2016@gmail.com>
2018-09-15 22:11:02 -0500

    Se sube avance proyecto web descriptiva realizado en Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { StartComponent } from './start/start.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes=[
+  {path:"", component: StartComponent},
+  {path:"start", component: StartComponent},
+  {path:"about", component: AboutComponent},
+  {path:"**", redirectTo: "/", pathMatch: "full"}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StartComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
